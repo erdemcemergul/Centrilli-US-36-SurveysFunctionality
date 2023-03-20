7b5bb77e2f368f8a59b5202fe4918bd995a14fa5
erdemcemergul <112733324+erdemcemergul@users.noreply.github.com>
2023-03-20 19:45:18 +0300

    Update CukesRunner.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -13,6 +13,9 @@ import org.junit.runner.RunWith;
                 "junit:target/junit/junit-report.xml",
                 "rerun:target/rerun.txt",},
         features = "src/test/resources/features",
+        glue = "com/cydeo/step_definition",
+        dryRun=false,
+        tags = "@surveys",
         publish = true
 
 )
